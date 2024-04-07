4b580cba7b2fb85fb8ee3396e55a9b87c0213ae2
Mateusz Marczyk <410191@student.agh.edu.pl>
2024-04-07 18:06:39 +0200

    code cleanup

--- OptimalSplit/src/main/java/com/ocado/basket/BasketSplitter.java
+++ OptimalSplit/src/main/java/com/ocado/basket/BasketSplitter.java
@@ -14,54 +14,12 @@ public class BasketSplitter {
             throw new RuntimeException("Error reading config file: " + e.getMessage());
         }
     }
-
-    public static List<List<Integer>> generatePermutations(int n, int size) {
-        List<List<Integer>> permutations = new ArrayList<>();
-        generatePermutationsHelper(n, size, new ArrayList<>(), permutations);
-        return permutations;
-    }
-
-    private static void generatePermutationsHelper(int n, int size, List<Integer> current, List<List<Integer>> permutations) {
-        if (n == 0 && current.size() == size) {
-            permutations.add(new ArrayList<>(current));
-            return;
-        }
-
-        if (current.size() >= size) {
-            return;
-        }
-
-        current.add(0);
-        generatePermutationsHelper(n, size, current, permutations);
-        current.remove(current.size() - 1);
-
-        if (n > 0) {
-            current.add(1);
-            generatePermutationsHelper(n - 1, size, current, permutations);
-            current.remove(current.size() - 1);
-        }
-    }
-    public static void displayHashMap(Map<String, Set<String>> map) {
-        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
-            String key = entry.getKey();
-            Set<String> values = entry.getValue();
-            System.out.println("Key: " + key + ", Values: " + values);
-        }
-    }
     private boolean checkIfAllItemsAreCovered(Map<String, Set<String>> delivery, Set<String> courierSet, List<String> items) {
         Set<String> itemsCovered = new HashSet<>();
         for (String courier : courierSet) {
             itemsCovered.addAll(delivery.get(courier));
         }
         return itemsCovered.containsAll(items);
-    }
-
-    public void displayItems(List<String> items) {
-        for (String item : items) {
-            System.out.println("ITEM " + item);
-            config.get(item).forEach(System.out::println);
-            System.out.println();
-        }
     }
 
     public List<HashSet<String>> generateSubsets(List<String> items, int n) {
@@ -78,19 +36,7 @@ public class BasketSplitter {
         for (int i = startIndex; i < items.size(); i++) {
             currentSubset.add(items.get(i));
             generateSubsetsHelper(items, n, i + 1, currentSubset, subsets);
-            currentSubset.remove(items.get(i)); // Remove the last added item to backtrack
-        }
-    }
-
-    private void generateSubsetsHelper(List<String> items, int n, int startIndex, List<String> currentSubset, List<List<String>> subsets) {
-        if (currentSubset.size() == n) {
-            subsets.add(new ArrayList<>(currentSubset));
-            return;
-        }
-        for (int i = startIndex; i < items.size(); i++) {
-            currentSubset.add(items.get(i));
-            generateSubsetsHelper(items, n, i + 1, currentSubset, subsets);
-            currentSubset.remove(currentSubset.size() - 1);
+            currentSubset.remove(items.get(i));
         }
     }
 
@@ -138,10 +84,7 @@ public class BasketSplitter {
         return result;
 
     }
-    //TODO: CHECK EDGE CASES - EACH COURIER HAS ONLY ONE ITEM
     public Map<String, List<String>> split(List<String> items) {
-        displayItems(items);
-
         HashMap<String, Set<String>> delivery = new HashMap<>();
         for (String item : items) {
             List<String> courier_list = config.get(item);
@@ -151,11 +94,10 @@ public class BasketSplitter {
             }
         }
         List<String> courierNames = new ArrayList<>(delivery.keySet());
-//        System.out.println(courierNames);
         boolean found = false;
         int curr_max = 0;
         Map<String, List<String>> curr_split = new HashMap<>();
-        for(int i = 1; i < courierNames.size(); i++) {
+        for(int i = 1; i <= courierNames.size(); i++) {
             if(found) {
                 break;
             }
@@ -171,26 +113,7 @@ public class BasketSplitter {
                     }
                 }
             }
-//            List<List<Integer>> permutations = generatePermutations(i, courierNames.size());
-//            for (List<Integer> permutation : permutations) {
-//                Set<String> courierSet = new HashSet<>();
-//                for (int j = 0; j < permutation.size(); j++) {
-//                    if (permutation.get(j) == 1) {
-//                        courierSet.add(courierNames.get(j));
-//                    }
-//                }
-//                if (checkIfAllItemsAreCovered(delivery, courierSet, items)) {
-//                    found = true;
-//                    courierSet.forEach(System.out::println);
-//                    return config;
-//
-//                }
-//            }
         }
         return splitMax(curr_split);
-    }
-
-    public String getConfig() {
-        return config.toString();
     }
 }
